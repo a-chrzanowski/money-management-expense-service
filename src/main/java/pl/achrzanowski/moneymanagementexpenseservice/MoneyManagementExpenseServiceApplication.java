45d75fcac7dee035b79eb38127c920bfbe68b1a3
aleksander.chrzanowski.dev <aleksander.chrzanowski.dev@gmail.com>
2022-08-03 16:21:54 +0200

    init commit

---
+++ src/main/java/pl/achrzanowski/moneymanagementexpenseservice/MoneyManagementExpenseServiceApplication.java
@@ -0,0 +1,13 @@
+package pl.achrzanowski.moneymanagementexpenseservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MoneyManagementExpenseServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MoneyManagementExpenseServiceApplication.class, args);
+	}
+
+}
